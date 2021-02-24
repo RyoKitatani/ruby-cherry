ee5f99243d3e4ffc6ac6b69fec7c2336de55d511
RyoKitatani <ryo0929m@gmail.com>
2021-02-24 22:48:12 +0000

    4-4完了

--- cp3.rb
+++ cp3.rb
@@ -11,32 +11,32 @@ a = ["apple", "orange", "melon"]
 # puts a[0]
 # puts a[0].length
 
-# a = ["lemon"]
-# puts a[0]
-# a[4] = ["cherry"]
-# puts a[4]
+# # a = ["lemon"]
+# # puts a[0]
+# # a[4] = ["cherry"]
+# # puts a[4]
+# # puts a[3]
+# a << 1
 # puts a[3]
-a << 1
-puts a[3]
-a.delete_at(1)
-puts a[1]
+# a.delete_at(1)
+# puts a[1]
 
-a,b = [1,2]
-puts a
-puts b
+# a,b = [1,2]
+# puts a
+# puts b
 
-c,d = [3]
-puts c
-puts d 
+# c,d = [3]
+# puts c
+# puts d 
 
-e,f = [100,200,300]
-puts e
-puts f
+# e,f = [100,200,300]
+# puts e
+# puts f
 
-puts 14.divmod(3)
+# puts 14.divmod(3)
 
-quo_rem = 14.divmod(3)
- puts "商=#{quo_rem[0]}, 余り=#{quo_rem[1]}"
+# quo_rem = 14.divmod(3)
+# puts "商=#{quo_rem[0]}, 余り=#{quo_rem[1]}"
 
 # 4-3 ブロック
 # numbers = [1,2,3,4]
@@ -47,19 +47,51 @@ quo_rem = 14.divmod(3)
 
 # puts sum
 
-a = [1,2,3,1,2,3]
-# a.delete(2)
-a.delete_if do |n|
-  n.odd?
-end
-puts a
+# a = [1,2,3,1,2,3]
+# # a.delete(2)
+# a.delete_if do |n|
+#   n.odd?
+# end
+# puts a
 
-numbers = [1,2,3,4]
+# numbers = [1,2,3,4]
+# sum = 0
+# numbers.each do |n|
+#   sum_value = n.even?? n*10:n
+#   sum += sum_value
+# end
+
+# puts sum
+
+# # 4-4 配列メソッド
+# numbers = [1,2,3,4]
+# new_numbers = []
+# numbers.each {|n| new_numbers << n * 10 }
+# puts new_numbers
+
+# mapメソッド
+
+numbers = [1,2,3,4,5]
+# new_numbers = numbers.map{|n| n * 10}
+# puts new_numbers
+
+# # selectメソッド
+# even_numbers = numbers.select{|n| n.even?}
+# puts even_numbers
+
+# # rejectメソッド
+# non_multiples_of_three = numbers.reject {|n| n % 3 == 0}
+# puts non_multiples_of_three
+
+# findメソッド
+even_numbers = numbers.find{|n| n.even?}
+puts even_numbers
+
+# injectメソッド
 sum = 0
-numbers.each do |n|
-  sum_value = n.even?? n*10:n
-  sum += sum_value
-end
-
+sum = numbers.inject(0){|result, n| result+n}
 puts sum
 
+weekends = ['Mon','Tue','Wed','Thu','Fri']
+day_of_weeks = weekends.inject('Sat'){|result,s|result + s}
+puts day_of_weeks
